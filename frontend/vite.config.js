44bf3db23c0d42cd2eddda27a58508fc577cc66e
Emadshenouda-hash <emadshenouda@gmail.com>
2025-08-02 15:34:46 +0300

    Fix: Use assetsInclude to load markdown files in Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "./", // ✅ Add this line
-  plugins: [react(), tailwindcss()],
+  base: "./",
+  plugins: [react()],
+  assetsInclude: ["**/*.md"], // ✅ add this
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
